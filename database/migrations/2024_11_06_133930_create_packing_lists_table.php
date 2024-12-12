62c9af39b64d9668849d14f3d467cf062c967c28
Konstantin Jovikj <jovichmk@gmail.com>
2024-12-12 16:56:01 +0100

    CONVERT DOCUMENTS FUNCTION MODIFIED

--- database/migrations/2024_11_06_133930_create_packing_lists_table.php
+++ database/migrations/2024_11_06_133930_create_packing_lists_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('packing_lists', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('owner_id')->constrained('companies');
-            $table->foreignId('client_id')->constrained('companies');
-            $table->foreignId('document_id')->nullable()->constrained('documents');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('owner_id')->constrained('companies')->onDelete('cascade');
+            $table->foreignId('client_id')->constrained('companies')->onDelete('cascade');
+            $table->foreignId('document_id')->nullable()->constrained('documents')->onDelete('cascade');
             $table->foreignId('vehicle_id')->nullable()->constrained('vehicles');
             $table->foreignId('driver_id')->nullable()->constrained('drivers');
             $table->foreignId('curency_id')->nullable()->constrained('curencies');
